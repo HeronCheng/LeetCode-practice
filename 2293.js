d13733f0c3e84464cd845945e12194cd7c4f4e67
HeronCheng <95325263+HeronCheng@users.noreply.github.com>
2023-01-02 00:21:34 +0800

    Create 2293.js

---
+++ 2293.js
@@ -0,0 +1,38 @@
+// 2293. Min Max Game
+
+/*
+You are given a 0-indexed integer array nums whose length is a power of 2.
+
+Apply the following algorithm on nums:
+
+Let n be the length of nums. If n == 1, end the process. Otherwise, create a new 0-indexed integer array newNums of length n / 2.
+For every even index i where 0 <= i < n / 2, assign the value of newNums[i] as min(nums[2 * i], nums[2 * i + 1]).
+For every odd index i where 0 <= i < n / 2, assign the value of newNums[i] as max(nums[2 * i], nums[2 * i + 1]).
+Replace the array nums with newNums.
+Repeat the entire process starting from step 1.
+Return the last number that remains in nums after applying the algorithm.
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minMaxGame = function(nums) {
+    if(nums.length === 1){
+        return nums[0];
+    }
+    let newArr = [];
+    for(let i = 0;i < nums.length;i++){
+
+        if(i % 2 !== 0){
+            continue;
+        }
+        else if(i % 4 === 0){
+            newArr.push(Math.min(nums[i], nums[i + 1]))
+        }
+        else{
+            newArr.push(Math.max(nums[i], nums[i + 1]))
+        }
+    }
+    return minMaxGame(newArr);
+};
