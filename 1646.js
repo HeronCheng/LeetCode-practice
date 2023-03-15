87ca6982920931ef0cebd6c501baef181f3617e8
HeronCheng <95325263+HeronCheng@users.noreply.github.com>
2023-03-15 23:13:34 +0800

    Create 1646.js

---
+++ 1646.js
@@ -0,0 +1,30 @@
+// 1646. Get Maximum in Generated Array
+// Easy
+
+/*
+You are given an integer n. A 0-indexed integer array nums of length n + 1 is generated in the following way:
+
+nums[0] = 0
+nums[1] = 1
+nums[2 * i] = nums[i] when 2 <= 2 * i <= n
+nums[2 * i + 1] = nums[i] + nums[i + 1] when 2 <= 2 * i + 1 <= n
+Return the maximum integer in the array nums​​​.
+*/
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var getMaximumGenerated = function(n) {
+    const arr = [0,1];
+    if (n < 2) return arr[n];
+    for (let i = 2; i <= n; i++) {
+        if (i%2 === 0){
+            arr[i] = arr[i/2];
+        }
+        else {
+            arr[i] = arr[(i-1)/2] + arr[(i-1)/2+1];
+        }
+    }
+    return Math.max(...arr);
+};
