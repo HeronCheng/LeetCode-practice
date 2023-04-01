a27dcde4afa2382a3cf6e6e2c4bb106ca2e6dc00
HeronCheng <95325263+HeronCheng@users.noreply.github.com>
2023-04-01 16:18:30 +0800

    Create 2024.js

---
+++ 2024.js
@@ -0,0 +1,45 @@
+// 2024. Maximize the Confusion of an Exam
+// Medium
+/*
+A teacher is writing a test with n true/false questions, with 'T' denoting true and 'F' denoting false. He wants to confuse the students by maximizing the number of consecutive questions with the same answer (multiple trues or multiple falses in a row).
+
+You are given a string answerKey, where answerKey[i] is the original answer to the ith question. In addition, you are given an integer k, the maximum number of times you may perform the following operation:
+
+Change the answer key for any question to 'T' or 'F' (i.e., set answerKey[i] to 'T' or 'F').
+Return the maximum number of consecutive 'T's or 'F's in the answer key after performing the operation at most k times.
+*/
+
+/**
+ * @param {string} answerKey
+ * @param {number} k
+ * @return {number}
+ */
+// timeout
+var maxConsecutiveAnswers = function(answerKey, k) {
+    const result = [];
+    if (answerKey.length === 1) {
+        return 1;
+    }
+    else {
+        for (let i = 0; i < answerKey.length; i++) {
+            let duplicate = answerKey;
+            let count = 1;
+            let leftOver = k;
+            for (let j = i; j < answerKey.length - 1; j++) {
+                if (duplicate[j] === duplicate[j+1]) {
+                    count ++;
+                }
+                else if (duplicate[j] !== duplicate[j+1] && leftOver > 0) {
+                    count ++;
+                    leftOver --;
+                    duplicate = duplicate.slice(0, j + 1) + duplicate[j] + duplicate.slice(j + 2);
+                }
+                else if (duplicate[j] !== duplicate[j+1] && leftOver === 0) {
+                    break;
+                }
+            }
+            result.push(count);
+        }
+        return Math.max(...result);
+    }
+};
