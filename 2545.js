6b57856358e40197a438d49cddb7cf45b3651368
HeronCheng <95325263+HeronCheng@users.noreply.github.com>
2023-05-11 23:28:59 +0800

    Create 2545.js

---
+++ 2545.js
@@ -0,0 +1,22 @@
+// 2545. Sort the Students by Their Kth Score
+// Medium
+/*
+There is a class with m students and n exams. You are given a 0-indexed m x n integer matrix score, where each row represents one student and score[i][j] denotes the score the ith student got in the jth exam. The matrix score contains distinct integers only.
+
+You are also given an integer k. Sort the students (i.e., the rows of the matrix) by their scores in the kth (0-indexed) exam from the highest to the lowest.
+
+Return the matrix after sorting it.
+*/
+
+/**
+ * @param {number[][]} score
+ * @param {number} k
+ * @return {number[][]}
+ */
+var sortTheStudents = function(score, k) {
+    return score.sort((a, b) => {
+        if (a[k] > b[k]) return -1;
+        if (a[k] < b[k]) return 1;
+        return 0;
+    })
+};
