ccd188a1b9a779cca6bcfc5e09123f38e64334d6
HeronCheng <95325263+HeronCheng@users.noreply.github.com>
2023-05-12 23:38:39 +0800

    Create 2667.js

---
+++ 2667.js
@@ -0,0 +1,18 @@
+// 2667. Create Hello World Function
+// Easy
+
+// Write a function createHelloWorld. It should return a new function that always returns "Hello World".
+
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    return function(...args) {
+        return "Hello World";
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
