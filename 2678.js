d1e549bc1a91fc56b2e729a506f1c480788ad79c
HeronCheng <95325263+HeronCheng@users.noreply.github.com>
2023-06-24 15:35:00 +0800

    Create 2678.js

---
+++ 2678.js
@@ -0,0 +1,18 @@
+// 2678. Number of Senior Citizens
+// Easy
+/*
+You are given a 0-indexed array of strings details. Each element of details provides information about a given passenger compressed into a string of length 15. The system is such that:
+
+The first ten characters consist of the phone number of passengers.
+The next character denotes the gender of the person.
+The following two characters are used to indicate the age of the person.
+The last two characters determine the seat allotted to that person.
+Return the number of passengers who are strictly more than 60 years old.
+*/
+/**
+ * @param {string[]} details
+ * @return {number}
+ */
+var countSeniors = function(details) {
+    return details.filter(person => Number(person.slice(11, 13)) > 60).length;
+};
